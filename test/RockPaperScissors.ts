b408f130815d52c242a9e24ba1ea1aba23b5aa46
edgar <garinich.7@gmail.com>
2022-10-05 23:46:20 +0400

    "should setup users and status" test

--- test/RockPaperScissors.ts
+++ test/RockPaperScissors.ts
@@ -4,6 +4,9 @@ import { Contract, ContractFactory } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 
+const emptyByte32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
+enum statuses { OFFER, REVOKED, DECLINED, MOVES, CANCELED, REVEALING, FINISHED, TIMEOUT }
+
 describe("RockPaperScissors.sol", () => {
   let contractFactory: ContractFactory;
   let contract: Contract;
@@ -35,6 +38,23 @@ describe("RockPaperScissors.sol", () => {
         .to.emit(contract, "GameUpdate")
         .withArgs(3, anyValue);
 
+    })
+
+    it("should setup users and status", async () => {
+      await contract.makeOffer(bob.address)
+      const game = await contract.games(0);
+
+      expect(game.player1.addr).to.equal(alice.address);
+      expect(game.player1.move).to.equal(emptyByte32);
+      expect(game.player1.secret).to.equal(0);
+      expect(game.player1.isClaimed).to.equal(false);
+
+      expect(game.player2.addr).to.equal(bob.address);
+      expect(game.player2.move).to.equal(emptyByte32);
+      expect(game.player2.secret).to.equal(0);
+      expect(game.player2.isClaimed).to.equal(false);
+
+      expect(game.status).to.equal(statuses.OFFER);
     })
 
     it("should start new game with 0 bet", async () => {
